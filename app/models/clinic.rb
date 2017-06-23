92b03d74db7ab4a51b596213f76b7022f5efb329
Arnon Hongklay <arnon@hongklay.com>
2017-06-23 21:51:43 +0700

    fixed order data

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -2,7 +2,7 @@ class Clinic < ApplicationRecord
   scope :feature, -> { where(flag: ['recommended', 'promotion']).limit(2) }
   scope :most_rating, -> { where.not(id: feature.select(:id)).order(rating: :desc).limit(3) }
 
-  scope :not_combined, -> { where.not(id: feature + most_rating) }
+  scope :not_combined, -> { where.not(id: feature + most_rating).order(created_at: :desc) }
 
   scope :combined, -> { feature + most_rating + not_combined }
 end
